ebd1d3afc5a707b13cfc52ee357f603abf32b21f
Sparsh Jain <41502834+SparshJain2000@users.noreply.github.com>
2020-10-01 01:26:22 +0530

    debug (string literals)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express =require("express"),
     request=require("request");
 
 app.get("/",(req,res)=>{
-    var url="http://data.fixer.io/api/latest?access_key=${process.env.apiKey}";
+    var url=`http://data.fixer.io/api/latest?access_key=${process.env.apiKey}`;
     request(url,(error,response,body)=>{
         var obj={"rupee":"INR","usd":"USD","pound":"GBP","euro":"EUR","yen":"JPY","riyal":"SAR"};
         var data=JSON.parse(body);
